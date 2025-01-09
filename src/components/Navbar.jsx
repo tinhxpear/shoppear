6b0c54d17a2587e48b1c472feefeae891abfb7a8
tinhxpear <tinhxpear@gmail.com>
2025-01-10 00:19:41 +0700

    Header Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,29 @@
+import { NavLink } from "react-router-dom";
 
-
-const Navbar = () => {
+const Navbar = ({containerStyles}) => {
+  const navLinks = [
+    { path: "/", title: "Home" },
+    { path: "/collection", title: "Collection" },
+    { path: "/blog", title: "Blog" },
+    { path: "mailto:info@shopear", title: "Contact" },
+  ];
   return (
-    <div>Navbar</div>
-  )
-}
+    <>
+      <nav className={`${containerStyles}`}>
+        {navLinks.map((link) => (
+          <NavLink
+            key={link.title}
+            to={link.path}
+            className={({ isActive }) =>
+              `${isActive ? "active-link" : ""} px-3 py-2 rounded-full`
+            }
+          >
+            {link.title}
+          </NavLink>
+        ))}
+      </nav>
+    </>
+  );
+};
 
-export default Navbar
+export default Navbar;
